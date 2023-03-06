c5f7778a6afc00e1839a21a2658d80ab0cea1f86
Navinraj.N <115686558+Navinraj05@users.noreply.github.com>
2023-03-06 22:15:19 +0530

    Add files via upload

---
+++ src/Details.js
@@ -0,0 +1,133 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import nodejs from "./assets/techstack/nodejs.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import expressjs from "./assets/techstack/expressjs.png";
+import mongodb from "./assets/techstack/mongodb.png";
+
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+
+
+import postman from "./assets/techstack/postman.png";
+
+// Porject Images
+import projectImage1 from "./assets/projects/project1.png";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Navin Raj.N",
+  img: profile,
+  about: `Enthusiastic graduate seeking to start a career as an entry-level software engineer with a reputed firm driven by technology. Proficient in  HTML 5, CSS, JavaScript, React JS, Node JS
+  Express JS, Mongo DB,  Passionate about implementing and launching new projects. And i am completed FULL STACK DEVELOPER course in GUVI`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/navinraj-n-280348226/",
+  github: "https://github.com/Navinraj05",
+  Email: "Navinraj5712@gmail.com",
+
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "DESKTOP SUPPORT ENGINEER",
+    Company: `kotak securities`,
+    Location: "CHENNAI",
+    Type: "Full Time",
+    Duration: "DEC 2020 - TILL NOW",
+  },
+
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "MERN STACK Developer",
+    CENTER: "GUVI,YouTube,",
+    Location: "Online",
+    Type: "Full Time",
+    Duration: "AUC 2022 - Present",
+  },
+  {
+    Position: "Bachelor in Electronics & Communication",
+    collage: `Your College Nam`,
+    Location: "CHENNAI",
+    Type: "Full Time",
+    Duration: "Aug 2016 - MAY 2020",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  bootstrap: bootstrap,
+  nodejs: nodejs,
+  expressjs: expressjs,
+  mongodb: mongodb,
+  vscode: vscode,
+  postman: postman,
+  github: github,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Open Brewery ",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+    Implementing the Open Brewery API using async/await with fetch.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://whimsical-ganache-0dca05.netlify.app/",
+    githubLink: "https://github.com/Navinraj05/webcode1.git",
+  },
+  {
+    title: "Equipment Hiring",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+    Equipment Hiring mern stack project  `,
+    techstack: "HTML/CSS, JavaScript,react,nodejs,expressjs,mongodb",
+    previewLink: "https://app.netlify.com/teams/navinraj05/sites",
+    githubLink: "https://github.com/Navinraj05?tab=repositories",
+  },
+  {
+    title: "mern stack project",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+    ______ mern stack project `,
+    techstack: "HTML/CSS, JavaScript,react,nodejs,expressjs,mongodb,",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+
+
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "Navinraj5712@gmail.com",
+  phone: "+91 9176757045",
+};
